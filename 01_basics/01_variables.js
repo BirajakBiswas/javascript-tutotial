8d297823c35a94b82c841b1253f61692772204ff
BirajakBiswas <115551368+BirajakBiswas@users.noreply.github.com>
2024-07-09 08:34:14 +0000

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 22234//const is used to declare constant value
+let accountEmail ="hhh@gmial.com"//let is used for variable values
+var accountPassword="2234radc"//var is also used for variable values but not preffered let is preferred
+accountCity="delhi"
+
+console.log(accountId);//used to display the value
+
+// accountId=222  //here accountId is constant so cannot be changed and shortcut to comment ctrl+/
+
+accountEmail="babaz@ffa.com"
+accountPassword="222rra133"
+accountCity="nagpur"
+
+console.table([accountId,accountEmail,accountPassword,accountPassword,accountCity])//used to dispaly all at once  in tabular format
+
+/*this 
+entire thing is a comment
+Prefer not to use var for variables
+becoz of issue in block scope and functional scope  
+*/
+
