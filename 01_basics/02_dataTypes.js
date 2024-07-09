6832a3a81af7003f454cde3124a1e3556958c8cb
BirajakBiswas <115551368+BirajakBiswas@users.noreply.github.com>
2024-07-09 09:44:06 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict";//treat all JS COde as newer version
+
+//alert(3+3)//used for browser,we are using nodejs so avoid this this will generate errot
+
+let name="bir"
+let age=19
+let isLoggedIn=false
+
+//number = upto 2 to the power 53
+//bigint for bigger case scneario not used normaly
+//string "" or '' both are ok prefer ""
+//boolean true/false
+//null-->stand alone value,let state = null is an example
+// undefined->let state; is undefined as state is not defined its basically undefined,value has not been assigned yet
+//symbol =>used for uniqueness
+//null is an object but undefined is a type of data
+
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
+
+ 
